4cd30ad6a28cafb45807a51a78ed01fa8f6b963f
divasatanica <jayhebes@vip.qq.com>
2017-07-13 22:46:39 +0800

    break:这个app.component只作为一个壳，仅作为组件容器和导航使用。

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,11 @@
 import { Component } from '@angular/core';
-import { Hero } from './hero-detail/Hero';
-import { HeroService } from './hero-detail/hero.service';
-
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  providers: [HeroService]
+    selector:'app-root',
+    templateUrl:'./app.component.html',
+    styleUrls:['./app.component.css'],
 })
 
-
-
 export class AppComponent {
-  aHeroes:Hero[];
-  sTitle = "Tour of Heroes";
-  oSelectedHero: Hero;
-  onSelect(hero: Hero): void {//ts规定的要写明函数返回值类型？
-    this.oSelectedHero = hero;
-  }
-  constructor(private heroService: HeroService) {};
-  getHeroes(): void {
-    this.heroService.getHeroes().then(heroes => this.aHeroes = heroes);
-  }
-  ngOnInit(): void {
-    this.getHeroes();
-  }
+    sTitle = 'Tour of Heroes';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HeroComponent } from './hero.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroService } from './hero-detail/hero.service';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
+    HeroComponent,
     HeroDetailComponent,
+    AppComponent,
+    DashboardComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'heroes',
+        component: HeroComponent,
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      },
+      {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+      }
+    ])
   ],
-  providers: [],
+  providers: [HeroService],
   bootstrap: [AppComponent]
 })
+
+
 export class AppModule { }
