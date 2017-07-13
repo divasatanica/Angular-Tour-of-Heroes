258e9d6cd6f0fbd86e53ddbda4e282d02ab2ec76
divasatanica <jayhebes@vip.qq.com>
2017-07-13 16:49:44 +0800

    将detail部分分为另外一个组件，创建了数据服务hero.service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,29 @@
 import { Component } from '@angular/core';
-export class Hero {
-  id: number;
-  name: string;
-}
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
+import { Hero } from './hero-detail/Hero';
+import { HeroService } from './hero-detail/hero.service';
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [HeroService]
 })
 
+
+
 export class AppComponent {
-  aHeroes = HEROES;
+  aHeroes:Hero[];
   sTitle = "Tour of Heroes";
   oSelectedHero: Hero;
   onSelect(hero: Hero): void {//ts规定的要写明函数返回值类型？
     this.oSelectedHero = hero;
+  }
+  constructor(private heroService: HeroService) {};
+  getHeroes(): void {
+    this.aHeroes = this.heroService.getHeroes();
+  }
+  ngOnInit(): void {
+    this.getHeroes();
   }
 }
